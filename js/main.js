42e1d8fc139c2f5ee05830154494cddd3161d9a1
AvvePavve <214724039+AvvePavve@users.noreply.github.com>
2025-09-01 14:14:20 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var map = new maplibregl.Map({
     container: 'map',
-    style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
+    style: 'https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf?key=AnloT5VXYX3E6sqkSTtS', // stylesheet location
     center: [13.011586184559851, 55.591988278009765], // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
