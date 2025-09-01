e7a8ffc2f7629427c245929dd84c03b86b5c636e
AvvePavve <214724039+AvvePavve@users.noreply.github.com>
2025-09-01 14:02:54 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var map = new maplibregl.Map({
     container: 'map',
     style: 'https://demotiles.maplibre.org/style.json', // stylesheet location
-    center: [55.591988278009765, 13.011586184559851], // starting position [lng, lat]
+    center: [13.011586184559851, 55.591988278009765], // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
