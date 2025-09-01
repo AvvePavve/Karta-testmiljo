e8018c143443fbe334b037b92e5c31a97e98cb46
AvvePavve <214724039+AvvePavve@users.noreply.github.com>
2025-09-01 14:23:53 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 var map = new maplibregl.Map({
     container: 'map',
-    style: 'https://api.maptiler.com/tiles/v3-openmaptiles/{z}/{x}/{y}.pbf?key=AnloT5VXYX3E6sqkSTtS', // stylesheet location
+    style: 'https://api.maptiler.com/maps/streets/style.json?key=AnloT5VXYX3E6sqkSTtS', // stylesheet location
     center: [13.011586184559851, 55.591988278009765], // starting position [lng, lat]
     zoom: 9 // starting zoom
     });
